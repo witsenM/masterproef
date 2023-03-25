ff4febaa36ffb6e4c5d80ab6942aafa44f2f562b
RPI <>
2023-03-25 13:56:24 +0100

    typo

--- neemFoto.py
+++ neemFoto.py
@@ -15,6 +15,6 @@ run(f"raspistill -t 1 -w 820 -h 616 -o {small_fp}")
 run(f"raspistill -t 1 -o {large_fp}")
 run(f"git add {small_fp}")
 run(f"git commit -m {small_fp}")
-run(f"git pull",)
-run(f"git push",)
+run(f"git pull")
+run(f"git push")
 
