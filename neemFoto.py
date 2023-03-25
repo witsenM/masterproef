ac2cc3ea62462f5d53bb3276f26c20e7a04cdad1
Geert Fannes <geert.fannes@gmail.com>
2023-03-25 12:33:20 +0100

    Support for small and large format pictures

--- neemFoto.py
+++ neemFoto.py
@@ -4,13 +4,17 @@ from datetime import datetime
 folder = "/home/pi/masterproef/fotos/"
 
 now = datetime.now()
-filename = now.strftime(f"{folder}foto-%Y-%m-%d-%Hu.jpg")
+small_fp = now.strftime(f"{folder}small-%Y-%m-%d-%Hu.jpg")
+large_fp = now.strftime(f"{folder}large-%Y-%m-%d-%Hu.jpg")
 
-cmd = f"raspistill -t 1 -o {filename}"
-subprocess.run(cmd, shell=True)
+def run(cmd):
+    print(f"Running {cmd}")
+    subprocess.run(cmd, shell=True)
 
-subprocess.run(f"git add {filename}")
-subprocess.run(f"git commit -m {filename}")
-subprocess.run(f"git pull")
-subprocess.run(f"git push")
+run(f"raspistill -t 1 -w 820 -h 616 -o {small_fp}")
+run(f"raspistill -t 1 -o {large_fp}")
+run(f"git add {small_fp}")
+run(f"git commit -m {small_fp}")
+run(f"git pull",)
+run(f"git push",)
 
