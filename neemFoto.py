412383eff3c47c7115e1e57c374d2d53ba64ac8a
Geert Fannes <geert.fannes@gmail.com>
2023-03-25 14:38:05 +0100

    Picture is taken each time until RPI runs longer than 5 minutes. After this, every 4 hours a picture will be taken

--- neemFoto.py
+++ neemFoto.py
@@ -2,22 +2,37 @@ import subprocess
 import os
 from datetime import datetime
 
-folder = "/home/pi/masterproef"
+def uptime():
+    with open('/proc/uptime', 'r') as f:
+        uptime_seconds = float(f.readline().split()[0])
+    return uptime_seconds
 
-os.chdir(folder)
+take_picture = False
+
+if uptime() <= 5*60:
+    take_picture = True
 
 now = datetime.now()
-small_fp = now.strftime(f"fotos/small-%Y-%m-%d-%H:%M.jpg")
-large_fp = now.strftime(f"fotos/large-%Y-%m-%d-%H:%M.jpg")
 
-def run(cmd):
-    print(f"Running {cmd}")
-    subprocess.run(cmd, shell=True)
+if now.time().hour % 4 == 0 and now.time().minute == 30:
+    take_picture = True
 
-run(f"raspistill -t 1 -w 820 -h 616 -o {small_fp}")
-run(f"raspistill -t 1 -o {large_fp}")
-run(f"git add {small_fp}")
-run(f"git commit -m {small_fp}")
-run(f"git pull")
-run(f"git push")
+if take_picture:
+    folder = "/home/pi/masterproef"
+
+    os.chdir(folder)
+
+    small_fp = now.strftime(f"fotos/small-%Y-%m-%d-%H:%M.jpg")
+    large_fp = now.strftime(f"fotos/large-%Y-%m-%d-%H:%M.jpg")
+
+    def run(cmd):
+        print(f"Running {cmd}")
+        subprocess.run(cmd, shell=True)
+
+    run(f"raspistill -t 1 -w 820 -h 616 -o {small_fp}")
+    run(f"raspistill -t 1 -o {large_fp}")
+    run(f"git add {small_fp}")
+    run(f"git commit -m {small_fp}")
+    run(f"git pull")
+    run(f"git push")
 
